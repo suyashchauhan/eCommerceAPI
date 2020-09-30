405f1a58445f384111cf20f5be92bdc8ff2c82df
Suyash Chauhan <suyashchauhan31@gmail.com>
2020-09-30 19:33:23 +0530

    cart routes and model

--- models/cart.js
+++ models/cart.js
@@ -4,6 +4,7 @@ const cartSchema = new mongoose.Schema(
     userId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Person",
+      required: true,
     },
     products: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
   },
--- routes/cart.js
+++ routes/cart.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { addtoCart } = require("../Controller/cart");
-router.route("/:id/add").get(addtoCart);
+const { addtoCart, removeCart, getCartItems } = require("../Controller/cart");
+const { protect } = require("../middleware/authorize");
+router.route("/:id/add").get(protect, addtoCart);
+router.route("/:id/remove").get(protect,removeCart);
+router.route("/").get(protect,getCartItems);
 module.exports = router;
