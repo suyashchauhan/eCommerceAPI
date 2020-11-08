c491b432f9530f48d455be70fd7153bf4326ddb1
Suyash Chauhan <suyashchauhan31@gmail.com>
2020-11-09 00:19:19 +0530

    MiddleWare for the  Add review route

--- middleware/authorize.js
+++ middleware/authorize.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const personModel = require("../models/person");
+const orderModel = require("../models/order");
 exports.authorize = (...roles) => {
   return (req, res, next) => {
     if (!req.user) {
@@ -24,10 +25,23 @@ exports.protect = async (req, res, next) => {
   if (!token) return next(new Error("couldn't extract token "));
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log(decoded);
     req.user = await personModel.findById(decoded.id);
     next();
   } catch (err) {
     return next(err);
   }
+};
+exports.canaddornot = async (req, res, next) => {
+  const hasPurchased = await orderModel.findOne({
+    isDelivered: true,
+    user: req.user._id,
+  });
+  if (!hasPurchased) {
+    return next(
+      new Error(
+        "the user has not purchased the product how can the user review it "
+      )
+    );
+  }
+  next();
 };
