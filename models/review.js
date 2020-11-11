ad23cfd0f35d92a3eeb995c6ea43fc300c436f29
Suyash Chauhan <suyashchauhan31@gmail.com>
2020-11-11 23:46:30 +0530

    Review Model updated

--- models/review.js
+++ models/review.js
@@ -10,10 +10,18 @@ const ReviewModel = new mongoose.Schema({
     min: [1, "Rating must be atleast 1"],
     max: [10, "Rating must be atleast 10"],
   },
-  user:{
-    type:mongoose.Schema.Types.ObjectId,
-    required:true,
-    ref:'Person'
-  }
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Person",
+  },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Product",
+  },
 });
+
+//ReviewModel.index({ user: 1, product: 1 }, { unique: true });
+
 module.exports = mongoose.model("Review", ReviewModel);
